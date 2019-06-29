b75fce3cb80bf1e75969ba2df5b4f4cbbaae4440
Marx JMoura <marx.jmoura@gmail.com>
2019-06-28 21:59:32 -0300

    Rename ufollow.Application to ufollow.API

--- src/ufollow.API/Startup.cs
+++ src/ufollow.API/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ufollow.Application
+namespace ufollow.API
 {
     public class Startup
     {
