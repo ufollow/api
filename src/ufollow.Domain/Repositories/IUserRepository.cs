5859e8d6a4b5dd6f6f0b35ba3c2fa1fe7f84b2a9
Marx JMoura <marx.jmoura@gmail.com>
2019-06-30 20:29:26 -0300

    Add user repository interface to domain layer

---
+++ src/ufollow.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using ufollow.Domain.Entities;
+
+namespace ufollow.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        Task<User> FindById(long id);
+        Task<User> FindByEmail(string email);
+        Task<bool> AnyUserWithEmail(string email);
+        void Remove(User user);
+    }
+}
