f898d262c4617899105b1cd12ba2a432d507c631
Marx JMoura <marx.jmoura@gmail.com>
2019-06-30 20:55:07 -0300

    Set Program class to sealed

--- src/ufollow.API/Program.cs
+++ src/ufollow.API/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ufollow.API
 {
-    public class Program
+    public sealed class Program
     {
         public static void Main(string[] args)
         {
